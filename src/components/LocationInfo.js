d06bced77281ed2331dc0090a23214b18bb490f4
sostraik1105 <sostraik1105@gmail.com>
2022-03-14 00:42:15 -0500

    :'v

--- src/components/LocationInfo.js
+++ src/components/LocationInfo.js
@@ -2,26 +2,6 @@ import React from 'react'
 
 export const LocationInfo = ({location}) => {
     return (
-<<<<<<< HEAD
-        <section className='info'>
-                <article>
-                    <h3>Name</h3>
-                    <p>{location.name}</p>
-                </article>
-                <article>
-                    <h3>Type</h3>
-                    <p>{location.type}</p>
-                </article>
-                <article>
-                    <h3>Dimension</h3>
-                    <p>{location.dimension}</p>
-                </article>
-                <article>
-                    <h3>Residents</h3>
-                    <p>{location.residents?.length}</p>                
-                </article>
-            </section>
-=======
         <section className='infos'>
             <div className='info_one'>
                 <article>
@@ -42,6 +22,5 @@ export const LocationInfo = ({location}) => {
                 </article>
             </div>
         </section>
->>>>>>> 47a3fd8deae5f41cdad5e94fe0eeb0fbb3c6b4c7
     )
 }
--- src/components/ResidentsList.js
+++ src/components/ResidentsList.js
@@ -4,11 +4,7 @@ import { Residents } from './Residents'
 export const ResidentsList = ({charList}) => {
 
     return (
-<<<<<<< HEAD
-        <div>
-=======
         <div className='card'>
->>>>>>> 47a3fd8deae5f41cdad5e94fe0eeb0fbb3c6b4c7
             {
                 charList?.map(el=>(
                     <Residents 
